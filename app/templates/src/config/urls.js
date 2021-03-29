0e93b2a7e6eefcae3f1661efd7dbd6ec48ebccb8
saarCiklum <saar@ciklum.com>
2021-03-29 01:47:42 -0700

    mocked end points added

--- app/templates/src/config/urls.js
+++ app/templates/src/config/urls.js
@@ -4,6 +4,7 @@ const { BASE_URL } = Config;
 
 export const adminApi = {
     getNodes: () => `${BASE_URL}/nodes/`,
-    //TODO replace with original nodes data
-    getDummyNodes: () => 'https://nodes.free.beeceptor.com/my/api/path'
+    //TODO replace with original nodes data end-point 
+    getDummyNodesB: () => 'https://nodes.free.beeceptor.com/my/api/path/nodes',
+    getDummyNodes: () => 'https://8b9df8a2-423e-4563-bbf6-529232e3dee5.mock.pstmn.io/localhost/5050/nodes'
 };
